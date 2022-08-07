afe9a9c9be78a94f23d5b4f25625b84feddd0746
zywan <finove@qq.com>
2022-08-08 00:07:44 +0800

    1. test gorm

--- go.mod
+++ go.mod
@@ -5,4 +5,9 @@ go 1.19
 require (
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/casbin/casbin/v2 v2.51.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.14 // indirect
+	gorm.io/driver/sqlite v1.3.6 // indirect
+	gorm.io/gorm v1.23.8 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	"github.com/casbin/casbin/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -18,5 +20,31 @@ func main() {
 	} else {
 		fmt.Printf("action deny\n")
 	}
+	UserDB()
 	fmt.Printf("done\n")
+}
+
+func UserDB() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Printf("failed to connect database")
+		return
+	}
+	db.AutoMigrate(&Device{})
+	db.Create(&Device{
+		MAC:    "000EA93D209C",
+		Remark: "测试1",
+	})
+	var dev Device
+	db.First(&dev, "mac = ?", "000EA93D209C")
+	log.Printf("find device %+v", dev)
+}
+
+type Device struct {
+	gorm.Model
+	MAC    string `json:"MAC"`
+	ID     string `json:"ID"`
+	Expire string `json:"Expire"`
+	Remark string `json:"Remark"`
+	Active bool   `json:"Active"`
 }
